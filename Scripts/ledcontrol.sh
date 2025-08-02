4096be0b1f681071afdbaaef78cacdbd691f9125
Nikita Zlobin <100912483+NikkyFreaky@users.noreply.github.com>
2025-08-02 23:21:28 +0700

    fix: syntax error

--- Scripts/ledcontrol.sh
+++ Scripts/ledcontrol.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-LED_STATUS='/sys/class/leds/blue:status/trigger'
+LED_STATUS="/sys/class/leds/blue:status/trigger"
 
 case "$1" in
     on)
-        echo "default-on" > $LED_STATUS"
+        echo "default-on" > "$LED_STATUS"
         ;;
+        
     off)
-        echo "none" > $LED_STATUS"
+        echo "none" > "$LED_STATUS"
         ;;
+        
     *)
         echo "Usage: $0 {on|off}"
         exit 1
